62649bd0471a4c666ebe8de15751e479d32fef0e
decryptoo <decryptmetoo@gmail.com>
2019-01-14 14:02:35 +0300

    stylus

--- js/app.tsx
+++ js/app.tsx
@@ -2,5 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './component/App';
 
+require('../css/app.styl');
+
 const app = document.querySelector('.app');
 ReactDOM.render(<App/>, app);
